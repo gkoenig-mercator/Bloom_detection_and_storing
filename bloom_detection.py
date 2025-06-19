096947e4dcd5c873b0717e3eb0db847e1b7cf766
gkoenig <gkoenig@mercator-ocean.fr>
2025-06-19 15:08:45 +0000

    Clean the bloom detection object and add some logic

--- bloom_detection.py
+++ bloom_detection.py
@@ -1,15 +1,70 @@
 import xarray as xr
 import numpy as np
 
+class BloomDetector :
 
-def determine_variable_statistics(dataset, variable):
-    return dataset[variable].mean(), dataset[variable].std()
+    phytoplankton_species_concentration = {'CHL' : {'baseline':0.02,
+                                                      'bloom':1.0,
+                                                    'duration':3},
+                                           'DIATO': {'baseline': 0.2,
+                                                     'bloom': 2.0,
+                                                     'duration':3},
+                                           'DINO': {'baseline': 0.5,
+                                                    'bloom':1.0,
+                                                    'duration':7},
+                                           'GREEN': {'baseline': 0.3,
+                                                     'bloom': 0.8,
+                                                     'duration':2},
+                                           'HAPTO': {'baseline':0.1,
+                                                     'bloom':1.2,
+                                                     'duration':7},
+                                           'MICRO': {'baseline': 0.3,
+                                                     'bloom': 2.0,
+                                                     'duration':3},
+                                           'NANO': {'baseline': 0.4,
+                                                    'bloom': 0.8,
+                                                    'duration':7},
+                                           'PICO': {'baseline':0.2,
+                                                    'bloom':0.5,
+                                                    'duration':1},
+                                           'PROCHLO': {'baseline':0.1,
+                                                       'bloom':0.3,
+                                                       'duration':7},
+                                           'PROKAR': {'baseline':0.2,
+                                                      'bloom':1.0,
+                                                      'duration':7}
+                                            }
 
-def detect_bloom(local_mean, local_std, global_mean, global_std):
-    if local_mean > global_mean + global_std and local_std < global_std:
-        return True
-    else :
-        return False
+    def __init__(self, dataset, global_mean_conc = None, global_std_conc = None, variable):
+        self.dataset = dataset
+        self.variable = variable
+        self.bloom_features_list = []
+        if global_mean_conc = None:
+            self.global_mean_conc = phytoplankton_species_concentration[variable]['baseline']
+        else:
+            self.global_mean_conc = global_mean_conc
+
+        if global_std_conc = None:
+            self.global_std_conc = 0.0
+        else:
+            self.global_std_conc = global_std_conc
+
+        self.local_mean_conc , self.local_std_conc = self.determine_variable_statistics()
+                 
+    def determine_variable_statistics(self):
+        return dataset[variable].mean(), dataset[variable].std()
+
+    def detect_bloom_statistically(self):
+        if self.local_mean > self.global_mean_conc + 2*self.local_std_conc:
+            return True
+        else:
+            return False
+
+    def detect_bloom_hard_threshold(self):
+        if self.local_mean > phytoplankton_species_concentration[self.variable]['bloom']:
+            return True
+        else:
+            return False
 
 def gather_bloom_features(sub_dataset, variables):
 
