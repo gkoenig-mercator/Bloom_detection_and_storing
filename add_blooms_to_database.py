854a1b17f5b134b9d616b8be90fcc9a0a11513b4
gkoenig <gkoenig@mercator-ocean.fr>
2025-06-20 10:00:38 +0000

    Clean the main script

--- add_blooms_to_database.py
+++ add_blooms_to_database.py
@@ -1,24 +1,40 @@
 import xarray as xr
 import numpy as np
 import os
+import utils
+import bloom_detection
+import plankton_database
+
+phytoplankton_species = ['CHL', 'DIATO', 'DINO','GREEN','HAPTO','MICRO','NANO','PICO','PROCHLO','PROKAR']
 
 database_url, database_name, username, password = ('postgresql-989098.user-gkoenig', 'defaultdb','user-gkoenig','fk7bu9vwjexxyv1jh39o')
 
-engine = connect_to_database(database_url,database_name, username, password)
-list_timestamps = generate_timestamps('2025-06-02','2025-06-17')
+plankton_DB = plankton_database.planktonDB(database_url,database_name, username, password)
+list_timestamps = utils.generate_timestamps('2025-06-02','2025-06-17')
 
 for i in range(len(list_timestamps)-1):
-    file_path, plankton_dataset = getting_plankton_dataset_specific_date(list_timestamps[i], list_timestamps[i+1])
-    blooms_CHL = scan_regions_for_blooms(plankton_dataset.isel(time=0), 'CHL', 10, 10)
+    file_path, plankton_dataset = utils.getting_plankton_dataset_specific_date(list_timestamps[i], list_timestamps[i+1])
+
+    global_mean_conc, global_std_conc = utils.determine_variable_statistics(plankton_dataset, 'CHL')
+
+    for subdataset in utils.subdatasets_extractor(plankton_dataset, 4):
+        bloom_detector = bloom_detection.BloomDetector(plankton_dataset,'CHL',
+                                                       global_mean_conc=global_mean_conc,
+                                                       global_std_conc=global_std_conc)
+
+        if bloom_detector.detect_bloom():
+            bloom_features =   {"Species":'Chl',
+                                 "Date":list_timestamps[i],
+                                 "Min_Lon": subdataset.longitude.min(),
+                                 "Max_Lon": subdataset.longitude.max(),
+                                 "Min_Lat": subdataset.latitude.min(),
+                                 "Max_Lat": subdataset.latitude.max(),
+                                 "Mean_Conc": bloom_detector.local_mean_conc,
+                                 "Std_Conc": bloom_detector.local_std_conc
+                                }
+            plankton_DB.insert_data(bloom_features)
+                                                       
+        
     os.remove(file_path)
 
-    for bloom in blooms_CHL:
-        insert_data(engine,bloom_table,'CHL',
-                    list_timestamps[i],
-                    bloom['min_lon'], 
-                    bloom['max_lon'], 
-                    bloom['min_lat'], 
-                    bloom['max_lat'], 
-                    bloom['mean_conc'], 
-                    bloom['std_conc'])
-    
+
